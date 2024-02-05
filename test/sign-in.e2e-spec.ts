a229aea12628c1a783848da5c5ff4ff425f7ffae
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2024-02-05 09:41:43 -0300

    test: add async assertions

--- test/sign-in.e2e-spec.ts
+++ test/sign-in.e2e-spec.ts
@@ -9,7 +9,8 @@ test('sign in successfully', async ({ page }) => {
   const toast = page.getByText(
     'Enviamos um link de autenticação para o seu e-mail.',
   )
-  expect(toast).toBeVisible()
+
+  await expect(toast).toBeVisible()
 })
 
 test('sign in with wrong credentials', async ({ page }) => {
@@ -19,12 +20,14 @@ test('sign in with wrong credentials', async ({ page }) => {
   await page.getByRole('button', { name: 'Acessar painel' }).click()
 
   const toast = page.getByText('Credenciais inválidas')
-  expect(toast).toBeVisible()
+
+  await expect(toast).toBeVisible()
 })
 
 test('navigate to new restaurant page', async ({ page }) => {
   await page.goto('/sign-in', { waitUntil: 'networkidle' })
 
   await page.getByRole('link', { name: 'Novo estabelecimento' }).click()
+
   expect(page.url()).toContain('/sign-up')
 })
--- test/sign-up.e2e-spec.ts
+++ test/sign-up.e2e-spec.ts
@@ -10,8 +10,9 @@ test('sign up successfully', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
 
-  const toast = page.getByText('Restaurante cadastrado com sucesso!')
-  expect(toast).toBeVisible()
+  const toast = page.getByText('Restaurante cadastrado com sucesso')
+
+  await expect(toast).toBeVisible()
 })
 
 test('sign up with error', async ({ page }) => {
@@ -24,8 +25,9 @@ test('sign up with error', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
 
-  const toast = page.getByText('Erro ao cadastrar restaurante.')
-  expect(toast).toBeVisible()
+  const toast = page.getByText('Erro ao cadastrar restaurante')
+
+  await expect(toast).toBeVisible()
 })
 
 test('navigate to login page', async ({ page }) => {
